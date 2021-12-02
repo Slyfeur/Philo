1b2391bb60bce88d04dd04fd96f836bd46733834
Thibault Uyttersprot <tuytters@fu-r7-p5.s19.be>
2021-12-02 14:11:17 +0100

    pair et impair gere

--- philo_clean/srcs/philo_utils.c
+++ philo_clean/srcs/philo_utils.c
@@ -6,7 +6,7 @@
 /*   By: tuytters <tuytters@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 10:44:38 by tuytters          #+#    #+#             */
-/*   Updated: 2021/12/02 12:07:38 by tuytters         ###   ########.fr       */
+/*   Updated: 2021/12/02 14:10:07 by tuytters         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,32 +31,26 @@ void	write_rout(char *txt, t_philo *philo, int pos)
 
 void	eat_philo(t_philo *philo)
 {
-	//printf("test1\n");
-	if (!(philo->env->philo_i[philo->pos].last_eat < philo->env->philo_i[(philo->pos - 1) % philo->env->nb_philo].last_eat || philo->env->philo_i[philo->pos].last_eat < philo->env->philo_i[(philo->pos + 1) % philo->env->nb_philo].last_eat))
-	{
-		ft_sleep(100, philo->env);
-	}
-	if (&philo->env->fork_i[philo->lfork] < &philo->env->fork_i[philo->rfork])
-	{
-		pthread_mutex_lock(&philo->env->fork_i[philo->lfork]);
-		//printf("test1ok\n");
-		write_rout("has taken a fork left", philo, philo->pos);
-		//printf("test2\n");
-		pthread_mutex_lock(&philo->env->fork_i[philo->rfork]);
-		//printf("test2ok\n");
-		write_rout("has taken a fork right", philo, philo->pos);
-	}
-	else
-	{
-		pthread_mutex_lock(&philo->env->fork_i[philo->rfork]);
-		//printf("test2ok\n");
-		write_rout("has taken a fork right", philo, philo->pos);
-		pthread_mutex_lock(&philo->env->fork_i[philo->lfork]);
-		//printf("test1ok\n");
-		write_rout("has taken a fork left", philo, philo->pos);
-	}
+	pthread_mutex_lock(&philo->env->fork_i[philo->lfork]);
+	write_rout("has taken a fork left", philo, philo->pos);
+	pthread_mutex_lock(&philo->env->fork_i[philo->rfork]);
+	write_rout("has taken a fork right", philo, philo->pos);
+	// if (&philo->env->fork_i[philo->lfork] < &philo->env->fork_i[philo->rfork])
+	// {
+	// 	pthread_mutex_lock(&philo->env->fork_i[philo->lfork]);
+	// 	write_rout("has taken a fork left", philo, philo->pos);
+	// 	pthread_mutex_lock(&philo->env->fork_i[philo->rfork]);
+	// 	write_rout("has taken a fork right", philo, philo->pos);
+	// }
+	// else
+	// {
+	// 	pthread_mutex_lock(&philo->env->fork_i[philo->rfork]);
+	// 	write_rout("has taken a fork right", philo, philo->pos);
+	// 	pthread_mutex_lock(&philo->env->fork_i[philo->lfork]);
+	// 	write_rout("has taken a fork left", philo, philo->pos);
+	// }
 	//pthread_mutex_lock(&philo->env->eat);
-	philo->env->philo_i[philo->pos].last_eat = (ft_time()
+	philo->env->philo_i[philo->pos - 1].last_eat = (ft_time()
 			- philo->env->time_to_go);
 	write_rout("is eating", philo, philo->pos);
 	philo->env->philo_eat++;
@@ -68,8 +62,18 @@ void	eat_philo(t_philo *philo)
 	
 	//pthread_mutex_unlock(&philo->env->eat);
 	ft_sleep(philo->env->time_eat, philo->env);
-	pthread_mutex_unlock(&philo->env->fork_i[philo->lfork]);
+	// if (&philo->env->fork_i[philo->lfork] < &philo->env->fork_i[philo->rfork])
+	// {
+	// 	pthread_mutex_unlock(&philo->env->fork_i[philo->lfork]);
+	// 	pthread_mutex_unlock(&philo->env->fork_i[philo->rfork]);
+	// }
+	// else
+	// {
+	// 	pthread_mutex_unlock(&philo->env->fork_i[philo->rfork]);
+	// 	pthread_mutex_unlock(&philo->env->fork_i[philo->lfork]);
+	// }
 	pthread_mutex_unlock(&philo->env->fork_i[philo->rfork]);
+	pthread_mutex_unlock(&philo->env->fork_i[philo->lfork]);
 }
 
 //void	max_eat_die(t_env *env, t_philo *philo)
@@ -82,18 +86,14 @@ void	*routine(void *param)
 
 	philo = (t_philo *) param;
 	env = philo->env;
-	/*time = ft_time() - philo->env->time_to_go;
-	if (time - (ft_time() - env->time_to_go) >= env->time_die)
-	{
-		env->die = 1;
-		return NULL;
-	}*/
-	if (philo->pos % 2 && env->nb_philo >= 1)
+	env->philo_i[philo->pos - 1].last_eat = ft_time() - env->time_to_go;
+	if ((philo->pos % 2) != 0)
 	{
 		write_rout("is thinking", philo, philo->pos);
-		ft_sleep(env->time_eat, env);
+		// ft_sleep(env->time_eat, env);
+		ft_sleep(env->time_eat / 50, env);
 	}
-	while (!env->die && env->eat_max)
+	while (/*!env->die &&*/ env->eat_max)
 	{
 		// if (((ft_time() - env->time_to_go) - philo->env->philo_i[philo->pos].last_eat) >= env->time_die)
 		// {
